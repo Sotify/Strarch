1f7b46dc701d84d04b4cc939083bffd0c9eb1914
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2020-07-09 13:45:04 +0000

    views/users/show.html.erbの6行目削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_133706) do
+ActiveRecord::Schema.define(version: 2020_07_09_134237) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "title"
     t.text "text"
     t.text "image"
