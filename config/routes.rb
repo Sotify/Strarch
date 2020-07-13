a0a3f8b93c786e97ad6591bc37376cfebabe1117
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2020-07-13 14:24:23 +0000

    config/routes.rbにpatchアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'posts/new' => 'posts#new'
   post 'posts' => 'posts#create'
   delete 'posts/:id' => 'posts#destroy'
+  patch 'posts/:id' => 'posts#update'
   get 'posts/:id/edit' => 'posts#edit'
   get 'users/:id' => 'users#show'
 end
