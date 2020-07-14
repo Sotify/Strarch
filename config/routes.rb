2e422dd40e94b663f7d6d1f5820e8e91c8b9a210
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2020-07-14 00:52:43 +0000

    config/routes.rbをresoucesメソッドを使った形に書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  get 'posts' => 'posts#index'
-  get 'posts/new' => 'posts#new'
-  post 'posts' => 'posts#create'
-  delete 'posts/:id' => 'posts#destroy'
-  patch 'posts/:id' => 'posts#update'
-  get 'posts/:id/edit' => 'posts#edit'
-  get 'users/:id' => 'users#show'
-  get 'posts/:id' => 'posts#show'
+  resources :posts
+  resources :users, only: [:show]
 end
