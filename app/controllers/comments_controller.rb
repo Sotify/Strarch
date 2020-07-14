f39068aaba1d23bc7fbfb71b47d830f3535e12b5
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2020-07-14 03:37:04 +0000

    controllers/comments_controllerにストロングパラメーターを指定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(text: params[:text], post_id: params[:post_id], user_id: current_user.id)
+    Comment.create(text: comment_params[:text], post_id: comment_params[:post_id], user_id: current_user.id)
+  end
+  
+  private
+  def comment_params
+    params.permit(:text, :post_id)
   end
 end
