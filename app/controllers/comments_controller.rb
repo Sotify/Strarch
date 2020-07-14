b6fd11d8b652c2ea125aa2a637c1c7a4d089ffbd
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2020-07-14 06:25:03 +0000

    controllers/comments_controllerのcreateアクションを編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(text: comment_params[:text], post_id: comment_params[:post_id], user_id: current_user.id)
+    comment = Comment.create(text: comment_params[:text], post_id: comment_params[:post_id], user_id: current_user.id)
+    redirect_to "/posts/#{comment.post.id}"
   end
   
   private
