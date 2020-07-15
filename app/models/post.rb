30cd7e90ec304e9170621f7a1ee859ae789af8ae
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2020-07-15 14:39:43 +0000

    models/post.rbにself.searchアクションを定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many   :comments
+  
+  def self.search
+    return Post.all unless search
+    Post.where(['content Like ?', "%#{search}%"])
+  end
 end
